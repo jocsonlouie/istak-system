5df076b5ef200baaf13a8fae074a79f81ba1b392
Louie Jocson <jocsonlouie10@gmail.com>
2022-10-31 10:17:05 +0800

    Configure PWA details

--- vue.config.js
+++ vue.config.js
@@ -22,4 +22,63 @@ module.exports = {
         .tap(opt => mergeSassVariables(opt, "'@/styles/variables.scss';"))
     })
   },
+  pwa: {
+    name: 'ISTAK Assumpta Dog & Cat Clinic',
+    themeColor: '#19ab6d',
+    msTileColor: '#19ab6d',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      "icons": [
+        {
+          "src": "./img/icons/icon-72x72.png",
+          "sizes": "72x72",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "./img/icons/icon-96x96.png",
+          "sizes": "96x96",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "./img/icons/icon-128x128.png",
+          "sizes": "128x128",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "./img/icons/icon-144x144.png",
+          "sizes": "144x144",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "./img/icons/icon-152x152.png",
+          "sizes": "152x152",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "./img/icons/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "./img/icons/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "./img/icons/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable any"
+        }
+      ]
+    }
+  }
 }
